d3256b761344879ac0182cb8f22160d1022fdba5
JMarts32 <jm2002.santosa@hotmail.com>
2024-11-11 10:53:41 -0500

    .

--- connection.py
+++ connection.py
@@ -24,7 +24,10 @@ from openai import OpenAI
 
 client = OpenAI()
 response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
+    model="gpt-3.5-turbo-0125",
     messages=messages
 )
 print(response.choices[0].message.content)
+
+
+# from langchain_core import PromptTemplates
